f8324e84ea47c9fd975d8e75d2b5a529c6a09fa9
Mark Romero <markromero@marks-mbp.lan>
2019-03-22 01:37:31 -0600

    fixed app 1

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
-  constuctor(props) {
-
+  constructor(props) {
+    super(props);
     this.state = {
       myName: 'Joe'
     }
   }
+
   render() {
     return (
       <div className="App">
