092903d743e076b43cc9fb0eaf6b72e3f140e7f2
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-03-23 10:49:11 -0600

    completed lodash

--- lodashPractice/lodash.js
+++ lodashPractice/lodash.js
@@ -75,8 +75,11 @@ var customers = [
 // first without using lodash's map (with property argument) method, then with it.
 
 //CODE HERE
+const emails = customers.map( customer => {
+  return customer.email;
+});
 
-
+const lodashEmails = _.map(customers,'email');
 
 var inviteList1 = ["Ed", "Fanny", "Mildred", "Alice", "James"];
 var inviteList2 = ["Jake", "Mildred", "Jimmy", "Ed", "Franklin"];
@@ -84,6 +87,21 @@ var inviteList2 = ["Jake", "Mildred", "Jimmy", "Ed", "Franklin"];
 // Uh oh! We are having a party and two invite lists were created.
 // Create a duplicate-free list of the people we want at the party without lodash's _.union().
 // Then create another list that removes all duplicates using _.union().
+function getNewList(listOne, listTwo) {
+  const newList = [...listOne];
+
+  for(let i = 0; i < listTwo.length; i++) {
+    if(!newList.includes(listTwo[i])) {
+      newList.push(listTwo[i]);
+    }
+  }
+  return newList;  
+}
+const newList = getNewList(inviteList1, inviteList2);
+
+//Lodash
+const lodashNewList = _.union(inviteList1, inviteList2);
+
 
 var friendsOfJim = [
   "Tom",
@@ -110,6 +128,31 @@ var friendsOfBetty = [
 // Jim and Betty are having a party, but they only want to invite mutual friends.
 // Create an array of mutual friends. First without using lodash.
 // Then using lodash's _.intersection().
+
+function findMutualFriends(arrays) {
+  //set the mutual friends equal to the first argument
+  const mutualFriends = arguments[0];
+  let length = mutualFriends.length;
+  //loop thorugh the remaining arrays
+  for(let i = 1; i < arguments.length; i++) {
+    //loop through the mutual firends array.
+    for(let j = 0; j < length; j++) {
+      //if the firend is not one of the other arrays then remove it
+      if(!arguments[i].includes(mutualFriends[j])) {
+        mutualFriends.splice(j, 1, false);
+      }
+    }
+  }
+  const finalList = mutualFriends.filter( friend => {
+    return friend;
+  })
+  return finalList;
+}
+
+const mutualFriends = findMutualFriends(friendsOfJim, friendsOfBetty);
+
+const lodashMutualFriends = _.intersection(friendsOfJim, friendsOfBetty);
+
 
 var purchases = [
   {
@@ -148,3 +191,25 @@ var purchases = [
 
 // First, group the purchases by company without lodash
 // then do it again using _.groupBy()
+
+function groupBy(purchases) {
+  const group = {};
+
+  //loop through the purchase, and pull out all the companies.
+  for(let i = 0; i < purchases.length; i++) {
+    const company = purchases[i].company;
+    //if the object doesnt have the company as a prop. then add it
+    if(!group.hasOwnProperty(company)) {
+      group[company] = [];
+      group[company].push(purchases[i]); 
+    } else {
+      group[purchases[i].company].push(purchases[i]);
+    }
+  }
+
+  return group;
+}
+
+const group = groupBy(purchases);
+
+const lodashGroup = _.groupBy(purchases, 'company');
